be97db35bc552c61c049be6d7aacd10abd814118
Winci <wincii@qq.com>
2024-03-01 09:13:34 +0800

    c

---
+++ train_script/pretrain_eva.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+LLM_VERSION=TinyLlama/TinyLlama-1.1B-Chat-v1.0
+# VT_VERSION=openai/clip-vit-base-patch16
+VT_VERSION=eva_clip/EVA02_CLIP_L_336_psz14_s6B.pt
+DATA_PATH=/root/autodl-tmp/code/llava/llava_pretrain/blip_laion_cc_sbu_558k.json
+IMAGE_PATH=/root/autodl-tmp/code/llava/llava_pretrain/images
+VT_VARIANT="${VT_VERSION#*/}"
+LLM_VARIANT="${LLM_VERSION#*/}"
+
+deepspeed tinyllava/train/train.py \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path $LLM_VERSION \
+    --version plain \
+    --data_path  $DATA_PATH\
+    --image_folder $IMAGE_PATH \
+    --vision_tower $VT_VERSION \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --tune_mm_mlp_adapter True \
+    --tune_entire_model False \
+    --fp16 True \
+    --output_dir ./checkpoints/tiny-llava-"${VERSION}"-"${LLM_VARIANT}"-"${VT_VARIANT}"-pretrain \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 2 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 24000 \
+    --save_total_limit 1 \
+    --learning_rate 1e-3 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 False \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 15 \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --run_name tiny-llava-"${VERSION}"-pretrain-"${LLM_VARIANT}"-"${VT_VARIANT}"
