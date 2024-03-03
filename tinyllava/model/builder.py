f596047df3ab3770008f219d005649d44e49e6b5
Winci <wincii@qq.com>
2024-03-03 21:54:00 +0800

    1

--- tinyllava/model/builder.py
+++ tinyllava/model/builder.py
@@ -126,8 +126,9 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
             tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False, padding_side="right")
             model = TinyLlavaPhiForCausalLM.from_pretrained(model_path, low_cpu_mem_usage=True, **kwargs)
         elif 'stablelm' in model_name.lower() or '2.0b' in model_name.lower():
-            from tinyllava.model.language_model.stablelm.tokenization_arcade100k import Arcade100kTokenizer
-            tokenizer = Arcade100kTokenizer.from_pretrained(model_path, use_fast=False, padding_side="right")
+            #from tinyllava.model.language_model.stablelm.tokenization_arcade100k import Arcade100kTokenizer
+           # tokenizer = Arcade100kTokenizer.from_pretrained(model_path, use_fast=False, padding_side="right")
+            tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
             model = TinyLlavaStablelmForCausalLM.from_pretrained(model_path, low_cpu_mem_usage=True, **kwargs)
         else:
             tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
